faded14a0e3cb4e481683d329b7c5b0d9b1f1938
Federica Bardini <federica.bardini03@gmail.com>
2018-09-25 00:59:11 +0200

    Added assertion to check that google actually displays some results

--- src/test/java/com/fb/uitestinghelloworld/StepDefinitions.java
+++ src/test/java/com/fb/uitestinghelloworld/StepDefinitions.java
@@ -1,16 +1,25 @@
 package com.fb.uitestinghelloworld;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class StepDefinitions {
+
     private WebDriver driver;
+
     @Given("^the Google home page$")
     public void the_Google_home_page() throws Exception {
         driver = new ChromeDriver();
@@ -28,7 +37,8 @@ public class StepDefinitions {
 
     @Then("^Google displays some results$")
     public void google_displays_some_results() {
+        List<WebElement> resultBoxes = driver.findElements(By.cssSelector("[data-hveid]"));
+        assertThat("number of results greater than 0",resultBoxes.size(), greaterThan(0));
         driver.quit();
     }
-
 }
